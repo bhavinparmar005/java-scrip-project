f50aec90e3b22355c1c60467190613ca4972ed2c
bhavinparmar005 <bhavinparmar005@gmail.com>
2024-10-10 16:31:46 -0700

    he

---
+++ asset/JS/script.js
@@ -0,0 +1,238 @@
+document.addEventListener('keydown',(e)=>{
+    if(e.key==='a' ||e.key==='A'){
+    document.getElementById('apple') .style.display = "block";
+    document.getElementById('ball') .style.display = "none";
+    document.getElementById('cat') .style.display = "none";
+    document.getElementById('dog') .style.display = "none";
+    document.getElementById('elephant') .style.display = "none";
+    document.getElementById('fish') .style.display = "none";
+    document.getElementById('grapes') .style.display = "none";
+    document.getElementById('horse') .style.display = "none";
+    document.getElementById('ice-cream') .style.display = "none";
+    document.getElementById('jug') .style.display = "none";
+    document.getElementById('kite') .style.display = "none";
+    document.getElementById('lion') .style.display = "none";
+    document.getElementById('monkey') .style.display = "none";
+    document.getElementById('nest') .style.display = "none";
+    document.getElementById('orange') .style.display = "none";
+    document.getElementById('peacock') .style.display = "none";
+    document.getElementById('queen') .style.display = "none";
+    document.getElementById('rose') .style.display = "none";
+    document.getElementById('sun') .style.display = "none";
+    document.getElementById('telephone') .style.display = "none";
+    document.getElementById('umbrella') .style.display = "none";
+    document.getElementById('van') .style.display = "none";
+    document.getElementById('watch') .style.display = "none";
+    document.getElementById('x-ray') .style.display = "none";
+    document.getElementById('yak') .style.display = "none";
+    document.getElementById('zebra') .style.display = "none";
+       
+    }else if(e.key==='b' ||e.key==='B'){
+        document.getElementById('apple') .style.display = "none";
+        document.getElementById('ball') .style.display = "block";
+        document.getElementById('cat') .style.display = "none";
+        document.getElementById('dog') .style.display = "none";
+        document.getElementById('elephant') .style.display = "none";
+        document.getElementById('fish') .style.display = "none";
+        document.getElementById('grapes') .style.display = "none";
+        document.getElementById('horse') .style.display = "none";
+        document.getElementById('ice-cream') .style.display = "none";
+        document.getElementById('jug') .style.display = "none";
+        document.getElementById('kite') .style.display = "none";
+        document.getElementById('lion') .style.display = "none";
+        document.getElementById('monkey') .style.display = "none";
+        document.getElementById('nest') .style.display = "none";
+        document.getElementById('orange') .style.display = "none";
+        document.getElementById('peacock') .style.display = "none";
+        document.getElementById('queen') .style.display = "none";
+        document.getElementById('rose') .style.display = "none";
+        document.getElementById('sun') .style.display = "none";
+        document.getElementById('telephone') .style.display = "none";
+        document.getElementById('umbrella') .style.display = "none";
+        document.getElementById('van') .style.display = "none";
+        document.getElementById('watch') .style.display = "none";
+        document.getElementById('x-ray') .style.display = "none";
+        document.getElementById('yak') .style.display = "none";
+        document.getElementById('zebra') .style.display = "none";
+
+
+    }else if(e.key==='c' ||e.key==='C'){
+        document.getElementById('apple') .style.display = "none";
+        document.getElementById('ball') .style.display = "none";
+        document.getElementById('cat') .style.display = "block";
+        document.getElementById('dog') .style.display = "none";
+        document.getElementById('elephant') .style.display = "none";
+        document.getElementById('fish') .style.display = "none";
+        document.getElementById('grapes') .style.display = "none";
+        document.getElementById('horse') .style.display = "none";
+        document.getElementById('ice-cream') .style.display = "none";
+        document.getElementById('jug') .style.display = "none";
+        document.getElementById('kite') .style.display = "none";
+        document.getElementById('lion') .style.display = "none";
+        document.getElementById('monkey') .style.display = "none";
+        document.getElementById('nest') .style.display = "none";
+        document.getElementById('orange') .style.display = "none";
+        document.getElementById('peacock') .style.display = "none";
+        document.getElementById('queen') .style.display = "none";
+        document.getElementById('rose') .style.display = "none";
+        document.getElementById('sun') .style.display = "none";
+        document.getElementById('telephone') .style.display = "none";
+        document.getElementById('umbrella') .style.display = "none";
+        document.getElementById('van') .style.display = "none";
+        document.getElementById('watch') .style.display = "none";
+        document.getElementById('x-ray') .style.display = "none";
+        document.getElementById('yak') .style.display = "none";
+        document.getElementById('zebra') .style.display = "none";
+
+
+    }else if(e.key==='d' ||e.key==='D'){
+        document.getElementById('apple') .style.display = "none";
+        document.getElementById('ball') .style.display = "none";
+        document.getElementById('cat') .style.display = "none";
+        document.getElementById('dog') .style.display = "block";
+        document.getElementById('elephant') .style.display = "none";
+        document.getElementById('fish') .style.display = "none";
+        document.getElementById('grapes') .style.display = "none";
+        document.getElementById('horse') .style.display = "none";
+        document.getElementById('ice-cream') .style.display = "none";
+        document.getElementById('jug') .style.display = "none";
+        document.getElementById('kite') .style.display = "none";
+        document.getElementById('lion') .style.display = "none";
+        document.getElementById('monkey') .style.display = "none";
+        document.getElementById('nest') .style.display = "none";
+        document.getElementById('orange') .style.display = "none";
+        document.getElementById('peacock') .style.display = "none";
+        document.getElementById('queen') .style.display = "none";
+        document.getElementById('rose') .style.display = "none";
+        document.getElementById('sun') .style.display = "none";
+        document.getElementById('telephone') .style.display = "none";
+        document.getElementById('umbrella') .style.display = "none";
+        document.getElementById('van') .style.display = "none";
+        document.getElementById('watch') .style.display = "none";
+        document.getElementById('x-ray') .style.display = "none";
+        document.getElementById('yak') .style.display = "none";
+        document.getElementById('zebra') .style.display = "none";
+
+
+    }else if(e.key==='e' ||e.key==='E'){
+        document.getElementById('apple') .style.display = "none";
+        document.getElementById('ball') .style.display = "none";
+        document.getElementById('cat') .style.display = "none";
+        document.getElementById('dog') .style.display = "none";
+        document.getElementById('elephant') .style.display = "block";
+        document.getElementById('fish') .style.display = "none";
+        document.getElementById('grapes') .style.display = "none";
+        document.getElementById('horse') .style.display = "none";
+        document.getElementById('ice-cream') .style.display = "none";
+        document.getElementById('jug') .style.display = "none";
+        document.getElementById('kite') .style.display = "none";
+        document.getElementById('lion') .style.display = "none";
+        document.getElementById('monkey') .style.display = "none";
+        document.getElementById('nest') .style.display = "none";
+        document.getElementById('orange') .style.display = "none";
+        document.getElementById('peacock') .style.display = "none";
+        document.getElementById('queen') .style.display = "none";
+        document.getElementById('rose') .style.display = "none";
+        document.getElementById('sun') .style.display = "none";
+        document.getElementById('telephone') .style.display = "none";
+        document.getElementById('umbrella') .style.display = "none";
+        document.getElementById('van') .style.display = "none";
+        document.getElementById('watch') .style.display = "none";
+        document.getElementById('x-ray') .style.display = "none";
+        document.getElementById('yak') .style.display = "none";
+        document.getElementById('zebra') .style.display = "none";
+
+
+    }else if(e.key==='f' ||e.key==='F'){
+        document.getElementById('apple') .style.display = "none";
+        document.getElementById('ball') .style.display = "none";
+        document.getElementById('cat') .style.display = "none";
+        document.getElementById('dog') .style.display = "none";
+        document.getElementById('elephant') .style.display = "none";
+        document.getElementById('fish') .style.display = "block";
+        document.getElementById('grapes') .style.display = "none";
+        document.getElementById('horse') .style.display = "none";
+        document.getElementById('ice-cream') .style.display = "none";
+        document.getElementById('jug') .style.display = "none";
+        document.getElementById('kite') .style.display = "none";
+        document.getElementById('lion') .style.display = "none";
+        document.getElementById('monkey') .style.display = "none";
+        document.getElementById('nest') .style.display = "none";
+        document.getElementById('orange') .style.display = "none";
+        document.getElementById('peacock') .style.display = "none";
+        document.getElementById('queen') .style.display = "none";
+        document.getElementById('rose') .style.display = "none";
+        document.getElementById('sun') .style.display = "none";
+        document.getElementById('telephone') .style.display = "none";
+        document.getElementById('umbrella') .style.display = "none";
+        document.getElementById('van') .style.display = "none";
+        document.getElementById('watch') .style.display = "none";
+        document.getElementById('x-ray') .style.display = "none";
+        document.getElementById('yak') .style.display = "none";
+        document.getElementById('zebra') .style.display = "none";
+
+
+    }else if(e.key==='g' ||e.key==='G'){
+        
+
+    }else if(e.key==='h' ||e.key==='H'){
+        
+
+    }else if(e.key==='i' ||e.key==='I'){
+        
+
+    }else if(e.key==='j' ||e.key==='J'){
+        
+
+    }else if(e.key==='k' ||e.key==='K'){
+        
+
+    }else if(e.key==='l' ||e.key==='L'){
+        
+
+    }else if(e.key==='m' ||e.key==='M'){
+        
+
+    }else if(e.key==='n' ||e.key==='N'){
+        
+
+    }else if(e.key==='o' ||e.key==='O'){
+        
+
+    }else if(e.key==='p' ||e.key==='P'){
+        
+
+    }else if(e.key==='q' ||e.key==='Q'){
+        
+
+    }else if(e.key==='r' ||e.key==='R'){
+        
+
+    }else if(e.key==='s' ||e.key==='S'){
+        
+
+    }else if(e.key==='t' ||e.key==='T'){
+        
+
+    }else if(e.key==='u' ||e.key==='U'){
+        
+
+    }else if(e.key==='v' ||e.key==='V'){
+        
+
+    }else if(e.key==='w' ||e.key==='W'){
+        
+
+    }else if(e.key==='x' ||e.key==='X'){
+        
+
+    }else if(e.key==='y' ||e.key==='Y'){
+        
+
+    }else if(e.key==='z' ||e.key==='Z'){
+        
+
+    }else{
+        alert("wrong key");
+    }
+})
